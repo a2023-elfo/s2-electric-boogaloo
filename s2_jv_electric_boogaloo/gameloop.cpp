5f8896a25e97d2e598110c1ad747dd372c9f31c1
MathieuMercier <merciermathieu412@gmail.com>
2024-04-11 11:07:41 -0400

    fic manette

--- s2_jv_electric_boogaloo/gameloop.cpp
+++ s2_jv_electric_boogaloo/gameloop.cpp
@@ -277,7 +277,7 @@ void Gameloop::mainLoop() {
             usecharge = false;
         }
         zombieMort.clear();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
         
     }
 }
